aee8279c65e24c6029ee4b139030328b112cb6a7
Abdelaziz Abdelioua <abdelaziz.abdelioua@airgreets.com>
2019-12-30 23:06:10 +0100

    first commit

---
+++ back-end/virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/abdelazizabdelioua/Desktop/project/back-end/virtualenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
